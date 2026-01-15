c2ead13eec0ffe29ca5541b37441e113f7bf7c98
karinfdez <karinfdez@yahoo.com>
2026-01-15 08:51:20 -0800

    Add types for data model

---
+++ app/lib/types.ts
@@ -0,0 +1,26 @@
+
+export type Stage =
+  | 'Applied'
+  | 'Screening'
+  | 'Interview'
+  | 'Offer'
+  | 'Hired'
+  | 'Rejected';
+
+
+
+// A record of an stage change
+export interface Activity {
+    id: string,
+    from?: Stage,
+    to: Stage,
+    timestamp: Date,
+}
+
+export interface Candidate {
+    id: string,
+    name: string,
+    role: string,
+    stage: Stage,  //current state
+    activities: Activity[]; // A timeline of stage changes
+}
